cac59a8cf3aa904db8ac98ff31a6b6f9900c6d9a
Takács Veronika <roni9764@gmail.com>
2020-11-11 22:46:58 +0100

    Fix link related bug in Navbar, refactor card display to use the BookCard component everywhere

--- src/components/layout/BooksOfAuthor.js
+++ src/components/layout/BooksOfAuthor.js
@@ -1,21 +1,18 @@
 import React, { useContext } from 'react';
 import '../../style/Cards.css';
 import { BookshelfContext } from "../../contexts/BookshelfContext"
+import BookCard from '../BookCard';
 
 function BooksOfAuthor(props) {
 
     const [bookshelf, setBookshelf] = useContext(BookshelfContext);
 
     const addToBookshelf = (currentBook) => {
-        const id = currentBook.id;
-        const author = currentBook.volumeInfo.author;
-        const title = currentBook.volumeInfo.title;
-        const smallThumbnail = currentBook.volumeInfo.imageLinks.smallThumbnail;
-        const bookToAdd = {id, author, title, smallThumbnail};
-        if (isBookAddedToBookshelf(id)) {
+        if (isBookAddedToBookshelf(currentBook.id)) {
             window.alert("Oops...You already added that book to your bookshelf!");
         } else {
-            setBookshelf([...bookshelf, bookToAdd]);
+            setBookshelf([...bookshelf, currentBook]);
+            console.log(currentBook);
         }
     }
 
@@ -23,14 +20,13 @@ function BooksOfAuthor(props) {
         let bookOnShelf = bookshelf.filter(book => book.id === id);
         return bookOnShelf.length > 0
     }
-    
+
     return (
         props.books
             .map(book => 
-                <div className="card" key={book.id}>
-                    <p><img className="book-smallthumbnail" alt={book.volumeInfo.title} src={book.volumeInfo.imageLinks.smallThumbnail} /></p>
-                    <p>{book.volumeInfo.title}</p>
-                    <button onClick={() => {addToBookshelf(book)}}>Add to Bookshelf</button>
+                <div>
+                <BookCard book={book} />
+                <button onClick={() => {addToBookshelf(book)}}>Add to Bookshelf</button>
                 </div>)
     )
 }
--- src/components/layout/Bookshelf.js
+++ src/components/layout/Bookshelf.js
@@ -1,23 +1,23 @@
 import React, { useContext } from "react";
 import { BookshelfContext } from "../../contexts/BookshelfContext";
+import BookCard from "../BookCard";
 
-export default function Bookshelf() {
-  const [bookshelf, setBookshelf] = useContext(BookshelfContext);
+const Bookshelf = () => {
+  const bookshelf = useContext(BookshelfContext)[0];
+
+  console.log(bookshelf);
 
   return (
     <div className="card-container">
-      {bookshelf.map((book) => (
-        <div className="card" key={book.id}>
-          <p>
-            <img
-              className="book-smallthumbnail"
-              alt={book.title}
-              src={book.smallThumbnail}
-            />
-          </p>
-          <p>{book.title}</p>
-        </div>
-      ))}
+      {
+      bookshelf.length > 0 ? 
+        (<div>
+          {bookshelf.map((book) => (
+            <BookCard book={book} key={book.id} />
+          ))}
+        </div>) : (<p>There are no books added to your bookshelf yet.</p>)}
     </div>
   );
 }
+
+export default Bookshelf;
