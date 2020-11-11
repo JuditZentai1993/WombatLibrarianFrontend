4b5c8dc52e0bcea78c5c99f9ff50d129b9db2919
Takács Veronika <roni9764@gmail.com>
2020-11-11 11:33:15 +0100

    Get books of specific author, basic display of booklist (first few results)

--- src/components/layout/AuthorDetails.js
+++ src/components/layout/AuthorDetails.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import BooksOfAuthor from "./BooksOfAuthor";
 
 export default function AuthorDetails() {
+
+  const [authorDetailsFromGoogle, setAuthorDetailsFromGoogle] = useState([]);
+  const [totalItems, setTotalItems] = useState(0);
+
+  useEffect(() => {
+    fetchTotalItemCount();
+    fetchAllBooksOfAuthor();
+  }, [])
+  
+  const fetchTotalItemCount = () => {
+    axios.get("https://www.googleapis.com/books/v1/volumes?q=inauthor:stephen+king&maxResults=1")
+      .then(response => {
+        setTotalItems(response.data.totalItems);
+      });
+  }
+
+  const fetchAllBooksOfAuthor = () => {
+    axios.get("https://www.googleapis.com/books/v1/volumes?q=inauthor:stephen+king&maxResults=40")
+    .then(response => {
+      setAuthorDetailsFromGoogle(response.data.items);
+    });
+  }
+
   return (
-    <div>
-      <h1>Author Details</h1>
+    <div className="card-container">
+      <BooksOfAuthor books={authorDetailsFromGoogle} />
     </div>
   );
 }
