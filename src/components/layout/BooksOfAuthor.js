535c2202b4b27c33de646731ee86594e756e3479
Takács Veronika <roni9764@gmail.com>
2020-11-11 14:53:35 +0100

    Add possibility to add books to the bookshelf

--- src/components/layout/BooksOfAuthor.js
+++ src/components/layout/BooksOfAuthor.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../../style/Cards.css';
 import { BookshelfContext } from "../../contexts/BookshelfContext"
 
 function BooksOfAuthor(props) {
 
+    const [bookshelf, setBookshelf] = useContext(BookshelfContext);
+
+    const addToBookshelf = (currentBook) => {
+        const id = currentBook.id;
+        const author = currentBook.volumeInfo.author;
+        const title = currentBook.volumeInfo.title;
+        const smallThumbnail = currentBook.volumeInfo.imageLinks.smallThumbnail;
+        const bookToAdd = {id, author, title, smallThumbnail};
+        if (isBookAddedToBookshelf(id)) {
+            window.alert("Oops...You already added that book to your bookshelf!");
+        } else {
+            setBookshelf([...bookshelf, bookToAdd]);
+        }
+    }
+
+    const isBookAddedToBookshelf = (id) => {
+        let bookOnShelf = bookshelf.filter(book => book.id === id);
+        return bookOnShelf.length > 0
+    }
+    
     return (
         props.books
-            .filter(book => book.volumeInfo.language === "en")
             .map(book => 
-                <div className="card">
-                    <img className="book-smallthumbnail" src={book.volumeInfo.imageLinks.smallThumbnail} />
+                <div className="card" key={book.id}>
+                    <p><img className="book-smallthumbnail" alt={book.volumeInfo.title} src={book.volumeInfo.imageLinks.smallThumbnail} /></p>
                     <p>{book.volumeInfo.title}</p>
+                    <button onClick={() => {addToBookshelf(book)}}>Add to Bookshelf</button>
                 </div>)
     )
 }
--- src/contexts/BookshelfContext.js
+++ src/contexts/BookshelfContext.js
@@ -3,8 +3,13 @@ import React, { useState, createContext } from 'react';
 export const BookshelfContext = createContext();
 
 export const BookshelfProvider = (props) => {
-    const [bookshelf, setBookshelf] = useState([]);
-    
+    const [bookshelf, setBookshelf] = useState([{
+        id: "iyTdDwAAQBAJ",
+        author: "Stephen King",
+        title: "Tortúra",
+        smallThumbnail: "http://books.google.com/books/content?id=iyTdDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api"
+    }]);
+
     return (
         <BookshelfContext.Provider value={[bookshelf, setBookshelf]}>
             {props.children}
