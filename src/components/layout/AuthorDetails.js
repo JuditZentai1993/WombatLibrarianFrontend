a360088de48c70ad6f34cb2e662bc71809f9e030
Takács Veronika <roni9764@gmail.com>
2020-11-12 19:02:00 +0100

    Add infinity scrolling to author details page

--- src/components/layout/AuthorDetails.js
+++ src/components/layout/AuthorDetails.js
@@ -2,11 +2,15 @@ import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
 import BooksOfAuthor from "./BooksOfAuthor";
+import InfiniteScroll from 'react-infinite-scroll-component';
+import wombatLoading from "../../images/wombat_chewing.gif";
 
 export default function AuthorDetails(props) {
 
   const [authorDetailsFromGoogle, setAuthorDetailsFromGoogle] = useState([]);
   const [totalItems, setTotalItems] = useState(0);
+  const maxResultsPerRequest = 40;
+  const [startIndex, setStartIndex] = useState(0);
 
   let author = useLocation();
 
@@ -23,15 +27,23 @@ export default function AuthorDetails(props) {
   }
 
   const fetchAllBooksOfAuthor = () => {
-    axios.get("https://www.googleapis.com/books/v1/volumes?q=" + author.state + "&maxResults=40")
+    axios.get("https://www.googleapis.com/books/v1/volumes?q=" + author.state 
+    + "&startIndex=" + startIndex + "&maxResults=" + maxResultsPerRequest)
     .then(response => {
-      setAuthorDetailsFromGoogle(response.data.items);
+      setAuthorDetailsFromGoogle([...authorDetailsFromGoogle, ...response.data.items]);
+      setStartIndex(startIndex + maxResultsPerRequest);
     });
   }
 
   return (
-    <div className="card-container">
-      <BooksOfAuthor books={authorDetailsFromGoogle} />
+    <div>
+      <InfiniteScroll className="card-container"
+        dataLength={authorDetailsFromGoogle.length} 
+        next={fetchAllBooksOfAuthor}
+        hasMore={authorDetailsFromGoogle.length < totalItems}
+        loader={<div><h4>Loading...</h4><img src={wombatLoading} alt="loading wombat" height="40%"/></div>}>
+          <BooksOfAuthor books={authorDetailsFromGoogle} />
+      </InfiniteScroll>
     </div>
   );
 }
