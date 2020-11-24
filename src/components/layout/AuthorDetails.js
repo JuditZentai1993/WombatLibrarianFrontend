18e1900dc2508a24d922b480152cb4bb8175142b
GZoltan22 <55540615+GZoltan22@users.noreply.github.com>
2020-11-24 16:40:20 +0100

    add vs to git ignore, modify to get book of author

--- src/components/layout/AuthorDetails.js
+++ src/components/layout/AuthorDetails.js
@@ -2,11 +2,10 @@ import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
 import BooksOfAuthor from "./BooksOfAuthor";
-import InfiniteScroll from 'react-infinite-scroll-component';
+import InfiniteScroll from "react-infinite-scroll-component";
 import wombatLoading from "../../images/wombat_chewing.gif";
 
 export default function AuthorDetails(props) {
-
   const [authorDetailsFromGoogle, setAuthorDetailsFromGoogle] = useState([]);
   const [totalItems, setTotalItems] = useState(0);
   const maxResultsPerRequest = 40;
@@ -18,18 +17,30 @@ export default function AuthorDetails(props) {
   useEffect(() => {
     fetchTotalItemCount();
     fetchBooksOfAuthor();
-  }, [])
-  
+  }, []);
+
   const fetchTotalItemCount = () => {
-    axios.get("https://www.googleapis.com/books/v1/volumes?q=inauthor:" + author.state + "&maxResults=1")
-      .then(response => {
+    axios
+      .get(
+        "https://www.googleapis.com/books/v1/volumes?q=inauthor:" +
+          author.state +
+          "&maxResults=1"
+      )
+      .then((response) => {
         setTotalItems(response.data.totalItems);
       });
-  }
+  };
 
   const fetchBooksOfAuthor = () => {
-    axios.get("https://www.googleapis.com/books/v1/volumes?q=inauthor:" + author.state 
-    + "&startIndex=" + startIndex + "&maxResults=" + maxResultsPerRequest)
+    axios
+      .get(
+        "https://localhost:5001/api/author/" +
+          author.state +
+          "&startIndex=" +
+          startIndex +
+          "&maxResults=" +
+          maxResultsPerRequest
+      )
     .then(response => {
       try {
         setAuthorDetailsFromGoogle([...authorDetailsFromGoogle, ...response.data.items]);
@@ -38,17 +49,37 @@ export default function AuthorDetails(props) {
         setHasMoreData(false)
       }
     });
-  }
+
+    // axios.get("https://www.googleapis.com/books/v1/volumes?q=inauthor:" + author.state
+    // + "&startIndex=" + startIndex + "&maxResults=" + maxResultsPerRequest)
+    // .then(response => {
+    //   try {
+    //     setAuthorDetailsFromGoogle([...authorDetailsFromGoogle, ...response.data.items]);
+    //     setStartIndex(startIndex + maxResultsPerRequest);
+    //   } catch {
+    //     setHasMoreData(false)
+    //   }
+    // });
+  };
 
   return (
     <div>
       <p>Total results: {totalItems}</p>
-      <InfiniteScroll className="card-container"
-        dataLength={authorDetailsFromGoogle.length} 
+      <InfiniteScroll
+        className="card-container"
+        dataLength={authorDetailsFromGoogle.length}
         next={fetchBooksOfAuthor}
         hasMore={authorDetailsFromGoogle.length < totalItems}
-        loader={hasMoreData ? (<div><h4>Loading...</h4><img src={wombatLoading} alt="loading wombat" height="40%"/></div>) : null}>
-          <BooksOfAuthor books={authorDetailsFromGoogle} />
+        loader={
+          hasMoreData ? (
+            <div>
+              <h4>Loading...</h4>
+              <img src={wombatLoading} alt="loading wombat" height="40%" />
+            </div>
+          ) : null
+        }
+      >
+        <BooksOfAuthor books={authorDetailsFromGoogle} />
       </InfiniteScroll>
     </div>
   );
