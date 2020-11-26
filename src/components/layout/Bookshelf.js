83285bbb99db7e653a8fbed977699b6d054696f4
GZoltan22 <55540615+GZoltan22@users.noreply.github.com>
2020-11-26 20:09:30 +0100

    bookshelf refact

--- src/components/layout/Bookshelf.js
+++ src/components/layout/Bookshelf.js
@@ -2,34 +2,36 @@ import React, { useContext } from "react";
 import { BookshelfContext } from "../../contexts/BookshelfContext";
 import BookCard from "../BookCard";
 import wombat from "../../images/wombat1.jpg";
+import axios from "axios";
 
 const Bookshelf = () => {
   const [bookshelf, setBookshelf] = useContext(BookshelfContext);
 
   const removeFromBookShelf = (id) => {
-    setBookshelf(bookshelf.filter(book => book.id !== id))
-  }
-
-  console.log("ITT A KAKII")
-  console.log(bookshelf);
+    setBookshelf(bookshelf.filter((book) => book.id !== id));
+  };
 
   return (
     <div>
-      {
-      bookshelf.length > 0 ? 
-        (<div className="card-container">
-          {bookshelf.map((book) => (
+      {bookshelf.length > 0 ? (
+        <div className="card-container">
+          {bookshelf[0].map((book) => (
             <div className="card-with-button">
-            <BookCard book={book} key={book.id} />
-            <button onClick={() => removeFromBookShelf(book.id)}>Remove</button>
+              <BookCard book={book} key={book.id} />
+              <button onClick={() => removeFromBookShelf(book.id)}>
+                Remove
+              </button>
             </div>
           ))}
-        </div>) : (<div>
+        </div>
+      ) : (
+        <div>
           <p>There are no books added to your bookshelf yet.</p>
-          <img src={wombat} alt="Cute wombat" width="20%"/>
-          </div>)}
+          <img src={wombat} alt="Cute wombat" width="20%" />
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default Bookshelf;
